4d2012739dfdcfe654eeb9091b635361c3e3f83f
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2026-07-11 18:24:39 +0530

    fix: let Vercel static builder serve frontend instead of Express

--- server/index.js
+++ server/index.js
@@ -35,11 +35,7 @@ app.get('/api/health', (req, res) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-});
 
 app.use(errorMiddleware);
 
